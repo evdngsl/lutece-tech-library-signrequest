c8c819c4c2dae3c6f59f0c759176626289d7101b
pierrelevy <pierrelevy@users.noreply.github.com>
2011-07-22 10:29:01 +0000

    SIGNREQUEST-2 : New Servlet Filter to validate SimpleHash signed requests
    git-svn-id: http://dev.lutece.paris.fr/svn/lutece/portal/trunk/plugins/library/library-signrequest@32454 bab10101-e421-0410-a517-8ce0973de3ef

---
+++ src/java/fr/paris/lutece/util/signrequest/servlet/SimpleHashSignRequestFilter.java
@@ -0,0 +1,113 @@
+/*
+ * Copyright (c) 2002-2011, Mairie de Paris
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  1. Redistributions of source code must retain the above copyright notice
+ *     and the following disclaimer.
+ *
+ *  2. Redistributions in binary form must reproduce the above copyright notice
+ *     and the following disclaimer in the documentation and/or other materials
+ *     provided with the distribution.
+ *
+ *  3. Neither the name of 'Mairie de Paris' nor 'Lutece' nor the names of its
+ *     contributors may be used to endorse or promote products derived from
+ *     this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * License 1.0
+ */
+package fr.paris.lutece.util.signrequest.servlet;
+
+import fr.paris.lutece.util.signrequest.SimpleHashAuthenticator;
+import fr.paris.lutece.util.signrequest.security.Sha1HashService;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * SimpleHash Sign Request Filter
+ */
+public class SimpleHashSignRequestFilter implements Filter
+{
+    private static final String PARAMETER_PRIVATE_KEY = "privateKey";
+    private static final String PARAMETER_ELEMENTS_SIGNATURE = "elementsSignature";
+    private static final String PARAMETER_VALIDITY_PERIOD = "validityTimePeriod";
+    private SimpleHashAuthenticator _authenticator;
+   
+    
+    /**
+     * {@inheritDoc }
+     */
+    public void init( FilterConfig filterConfig ) throws ServletException
+    {
+        _authenticator = new SimpleHashAuthenticator();
+        
+        // Set the Hashing service
+        _authenticator.setHashService( new Sha1HashService() );
+        
+        // Set the shared secret between client and server
+        String strPrivateKey = filterConfig.getInitParameter( PARAMETER_PRIVATE_KEY);
+        _authenticator.setPrivateKey( strPrivateKey );
+        
+        // Set the list of elements that compose the signature
+        String strElementsList = filterConfig.getInitParameter( PARAMETER_ELEMENTS_SIGNATURE );
+        StringTokenizer st = new StringTokenizer( strElementsList , "," );
+        List<String> listElements = new ArrayList<String>();
+        while( st.hasMoreTokens() )
+        {
+            listElements.add( st.nextToken().trim() );
+        }
+        _authenticator.setSignatureElements( listElements );                
+         
+        // Sets The validity Time Period
+        String strValidityTimePeriod = filterConfig.getInitParameter( PARAMETER_VALIDITY_PERIOD );
+        _authenticator.setValidityTimePeriod( Long.parseLong( strValidityTimePeriod ) );
+        
+    }
+
+    /**
+     * {@inheritDoc }
+     */
+    public void doFilter( ServletRequest request, ServletResponse response, FilterChain chain ) throws IOException, ServletException
+    {
+        if (_authenticator.isRequestAuthenticated( (HttpServletRequest) request ) )
+        {
+            chain.doFilter( request, response );
+        } else
+        {
+            ((HttpServletResponse) response).setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        }
+    }
+
+    /**
+     * {@inheritDoc }
+     */
+    public void destroy()
+    {
+    }
+    
+}
