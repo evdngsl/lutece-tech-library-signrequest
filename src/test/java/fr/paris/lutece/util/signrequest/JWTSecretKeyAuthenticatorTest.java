d771902b0ff5f7fffba233b22f81383fbce195cd
Alexandre Close <alexandre.close@gmail.com>
2019-03-18 18:02:21 +0100

    SIGNREQUEST-12 : Add two test classes for test signing of requests with JWT (no encryption and with secret key encryption)

---
+++ src/test/java/fr/paris/lutece/util/signrequest/JWTSecretKeyAuthenticatorTest.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) 2002-2016, Mairie de Paris
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  1. Redistributions of source code must retain the above copyright notice
+ *     and the following disclaimer.
+ *
+ *  2. Redistributions in binary form must reproduce the above copyright notice
+ *     and the following disclaimer in the documentation and/or other materials
+ *     provided with the distribution.
+ *
+ *  3. Neither the name of 'Mairie de Paris' nor 'Lutece' nor the names of its
+ *     contributors may be used to endorse or promote products derived from
+ *     this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * License 1.0
+ */
+
+package fr.paris.lutece.util.signrequest;
+
+import fr.paris.lutece.test.MokeHttpServletRequest;
+import fr.paris.lutece.util.jwt.service.JWTUtil;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.commons.httpclient.Header;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+/**
+ * JWTSecretKeyAuthenticatorTest
+ */
+public class JWTSecretKeyAuthenticatorTest
+{
+    private static final String CLAIM_KEY = "claim_key";
+    private static final String CLAIM_VALUE = "claim_value";
+    private static final String HTTP_HEADER_NAME = "header_name";
+    private static final String SECRET_KEY = "testestestestest";
+    private static final long VALIDITY = 60000;
+
+    /**
+     * Test of isRequestAuthenticated method, of class JWTSecretKeyAuthenticator.
+     */
+    @Test
+    public void testSignRequestAndTestAuth( )
+    {
+        MokeHttpServletRequest request = new MokeHttpServletRequest( );
+
+        Map<String, String> mapJWTClaims = new HashMap<>( );
+        mapJWTClaims.put( CLAIM_KEY, CLAIM_VALUE );
+
+        JWTSecretKeyAuthenticator authenticator = new JWTSecretKeyAuthenticator( mapJWTClaims, HTTP_HEADER_NAME, VALIDITY, null, SECRET_KEY );
+        authenticator.setValidityTimePeriod( VALIDITY );
+
+        request.addMokeHeader( HTTP_HEADER_NAME, JWTUtil.buildBase64JWT( mapJWTClaims, authenticator.getExpirationDate( ), null, SECRET_KEY ) );
+
+        assertTrue( authenticator.isRequestAuthenticated( request ) );
+        assertTrue( JWTUtil.checkPayloadValues( request, HTTP_HEADER_NAME, mapJWTClaims ) );
+    }
+}
